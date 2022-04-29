2a8392b849452d1f3770582456e6583f2c57d2f9
Can Göktas <can.goektas@gmail.com>
2022-04-29 11:20:25 +0200

    WIP

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -5,8 +5,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
 	"net/http"
+	"net/http/httputil"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,21 +17,63 @@ import (
 
 	"github.com/cangoektas/go-open-telemetry/internal/helper"
 	"github.com/cangoektas/go-open-telemetry/pkg/registry"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 var reg *registry.Registry
+var randPort int
+var name string
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+	var (
+		min = 1025
+		max = 65535
+	)
+	randPort = rand.Intn(max-min) + min
+	name = os.Getenv("NAME")
 }
 
 var serviceDiscoveryAddr = "http://localhost:8090"
 
 func main() {
-	min := 1025
-	max := 65535
-	randPort := rand.Intn(max-min) + min
-	name := os.Getenv("NAME")
+	l := log.New(os.Stdout, "", 0)
+	// Write telemetry data to a file.
+	f, err := os.Create(fmt.Sprintf("%s-traces.txt", name))
+	if err != nil {
+		l.Fatal(err)
+	}
+	defer f.Close()
+
+	exp, err := newExporter(f)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	tc := propagation.TraceContext{}
+	// Register the TraceContext propagator globally.
+	otel.SetTextMapPropagator(tc)
+	otelhttp.WithPropagators(tc)
+
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(newResource()),
+	)
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	otel.SetTracerProvider(tp)
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -36,6 +81,12 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/registry", setRegistry)
+
+	helloHandler := http.HandlerFunc(hello)
+	wrapperHelloHandler := otelhttp.NewHandler(helloHandler, "hello-instrumented")
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		wrapperHelloHandler.ServeHTTP(w, req)
+	})
 	server := &http.Server{Addr: fmt.Sprintf("localhost:%d", randPort), Handler: mux}
 
 	go func() {
@@ -56,7 +107,10 @@ func main() {
 		done <- true
 	}()
 
-	registerService(name, fmt.Sprintf("localhost:%d", randPort))
+	localAddr := fmt.Sprintf("localhost:%d", randPort)
+
+	fmt.Printf("Starting server at %s\n", localAddr)
+	registerService(name, localAddr)
 
 	<-done
 }
@@ -94,4 +148,76 @@ func setRegistry(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	newCtx, span := otel.Tracer(name).Start(req.Context(), "hello")
+
+	defer req.Body.Close()
+	reqDump, err := httputil.DumpRequest(req, false)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(reqDump))
+
+	if len(reg.AddrByName) == 1 || rand.Float32() < 0.1 {
+		w.Write([]byte("Hello, world!\n"))
+		span.End()
+		return
+	}
+
+	// Sleep up to 250 milliseconds
+	time.Sleep(time.Duration(rand.Intn(251)) * time.Millisecond)
+
+	addrs := make([]string, 0)
+	for serviceName, addr := range reg.AddrByName {
+		if serviceName != name {
+			addrs = append(addrs, addr)
+		}
+	}
+
+	randIndex := rand.Intn(len(addrs))
+	randAddr := addrs[randIndex]
+
+	nextReq, err := http.NewRequest(http.MethodGet, "http://"+randAddr+"/hello", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	nextReq = nextReq.WithContext(newCtx)
+	defaultClient := http.DefaultClient
+
+	resp, err := defaultClient.Do(nextReq)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(w, resp.Body)
+	span.End()
+}
+
+// newExporter returns a console exporter.
+func newExporter(w io.Writer) (trace.SpanExporter, error) {
+	return stdouttrace.New(
+		stdouttrace.WithWriter(w),
+		// Use human-readable output.
+		stdouttrace.WithPrettyPrint(),
+		// Do not print timestamps for the demo.
+		stdouttrace.WithoutTimestamps(),
+	)
+}
+
+// newResource returns a resource describing this application.
+func newResource() *resource.Resource {
+	r, _ := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(name),
+			semconv.ServiceVersionKey.String("v0.1.0"),
+			attribute.String("environment", "demo"),
+		),
+	)
+	return r
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,16 @@ module github.com/cangoektas/go-open-telemetry
 go 1.18
 
 replace github.com/cangoektas/go-open-telemetry => ./
+
+require (
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.31.0 // indirect
+	go.opentelemetry.io/otel v1.6.3 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.6.3 // indirect
+	go.opentelemetry.io/otel/metric v0.28.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.6.3 // indirect
+	go.opentelemetry.io/otel/trace v1.6.3 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+)
